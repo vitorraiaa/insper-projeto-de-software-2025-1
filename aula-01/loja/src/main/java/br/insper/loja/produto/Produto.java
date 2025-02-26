2517e527c8aac65ef5689b62cf22c00225ba7685
vitorraiaa <vitortraia@gmail.com>
2025-02-26 15:09:06 -0300

    fix

---
+++ aula-01/loja/src/main/java/br/insper/loja/produto/Produto.java
@@ -0,0 +1,43 @@
+package br.insper.loja.produto;
+
+import org.springframework.data.annotation.Id;
+
+public class Produto {
+    @Id
+    private String id;
+    private String nome;
+    private double preco;
+    private int quantidadeEmEstoque;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+}
